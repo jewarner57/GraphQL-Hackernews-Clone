0a32408e89cfb953653ad322c3db24ff9f17c789
Jonathan Warner <jewarner57@gmail.com>
2022-02-10 20:19:47 -0500

    feat: create a feed query to get all links

--- schema/index.ts
+++ schema/index.ts
@@ -1,10 +1,11 @@
-import { makeSchema } from 'nexus'
-import { join } from 'path'
+import { makeSchema } from "nexus";
+import { join } from "path";
+import * as types from "../src/graphql";   // 1
 
 export const schema = makeSchema({
-  types: [], // 1
+  types,   // 2
   outputs: {
-    schema: join(__dirname, '..', 'schema.graphql'), // 2
-    typegen: join(__dirname, '..', 'nexus-typegen.ts'), // 3
+    typegen: join(__dirname, "..", "nexus-typegen.ts"),
+    schema: join(__dirname, "..", "schema.graphql"),
   },
-})
+});
