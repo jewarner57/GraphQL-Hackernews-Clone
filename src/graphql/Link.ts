ac3f6642dba46e93d98365137c97cc1ac98cefd3
Jonathan Warner <jewarner57@gmail.com>
2022-02-11 16:23:21 -0500

    feat: add create-link mutation and get link query

--- src/graphql/Link.ts
+++ src/graphql/Link.ts
@@ -1,5 +1,5 @@
-import { extendType, objectType } from "nexus";
 import { NexusGenObjects } from "../../nexus-typegen";  
+import { extendType, intArg, nonNull, objectType, stringArg } from "nexus";   
 
 export const Link = objectType({
   name: "Link", // 1 
@@ -30,6 +30,50 @@ export const LinkQuery = extendType({  // 2
       type: "Link",
       resolve(parent, args, context, info) {    // 4
         return links;
+      },
+    });
+  },
+});
+
+export const LinkIdQuery = extendType({
+  type: "Query",
+  definition(t) {
+    t.field("link", {
+      type: "Link",
+      args: {
+        id: nonNull(intArg())
+      },
+      resolve(parent, args, context, info) {
+        const {id} = args
+        const link = links.find(obj => obj.id === id) || null
+
+        return link;
+      },
+    });
+  },
+});
+
+export const LinkMutation = extendType({  // 1
+  type: "Mutation",
+  definition(t) {
+    t.nonNull.field("post", {  // 2
+      type: "Link",
+      args: {   // 3
+        description: nonNull(stringArg()),
+        url: nonNull(stringArg()),
+      },
+
+      resolve(parent, args, context) {
+        const { description, url } = args;  // 4
+
+        let idCount = links.length + 1;  // 5
+        const link = {
+          id: idCount,
+          description: description,
+          url: url,
+        };
+        links.push(link);
+        return link;
       },
     });
   },
